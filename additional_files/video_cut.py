a6e612f36ec2bdd724b4cac303609798d9ce994e
BLGALEX <lepehaleha@bk.ru>
2021-11-15 12:32:19 +0300

    VideoTransformer added and method transform is ready

--- additional_files/video_cut.py
+++ additional_files/video_cut.py
@@ -1,3 +1,32 @@
+import cv2 as cv
 
 
-pass
+class VideoTransformer:
+
+    def __init__(self, fps=30, height=100, width=100, is_crop=False):
+        self.fps = fps
+        self.height = height
+        self.width = width
+
+        self.is_crop = is_crop
+
+    def transform(self, path, inplace=False):
+        cap = cv.VideoCapture(path)
+        if not inplace:
+            path, _ = path.split(sep='.')
+            path += '_transformed'+'.'+_
+        out = cv.VideoWriter(path, -1, self.fps, (self.width, self.height))
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if ret:
+                dim = (self.width, self.height)
+                frame_re = cv.resize(frame, dim)
+                out.write(frame_re)
+            else:
+                break
+        cap.release()
+        out.release()
+        cv.destroyAllWindows()
+
+    def get_cut(self, path):
+        pass
