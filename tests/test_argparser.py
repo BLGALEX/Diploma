92da9cfab1d746bdad166b17a243865186d8206f
BLGALEX <lepehaleha@bk.ru>
2022-05-03 17:43:56 +0900

    tests added

---
+++ tests/test_argparser.py
@@ -0,0 +1,13 @@
+import os
+from subprocess import Popen, PIPE
+
+
+
+def test_argsparsing():
+    path =  os.path.abspath(__file__ + "/../../source/main.py")
+
+    p = Popen(['python', path, '123245/fwebrg/vfeve'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    output, err = p.communicate()
+    rc = p.returncode
+    assert '123245/fwebrg/vfeve' in output.decode("utf-8")
+    assert rc == 0
