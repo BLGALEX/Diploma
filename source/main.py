4a6efe2517a4e448e858e9686a8fe242890aaebd
BLGALEX <lepehaleha@bk.ru>
2022-05-03 17:27:56 +0900

    refactored project structure

---
+++ source/main.py
@@ -0,0 +1,10 @@
+import argparse
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Script for video stabilization estimation')
+    parser.add_argument('path', metavar='videofile', type=str,
+                        help='path to videofile to estimate')
+
+    args = parser.parse_args()
+    print(args.path)
