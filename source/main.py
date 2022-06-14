291f5c774fc4346f3f78ad7a440ddbc57e2f875b
BLGALEX <lepehaleha@bk.ru>
2022-06-14 19:05:15 +1000

    connfigured argparser

--- source/main.py
+++ source/main.py
@@ -3,10 +3,35 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description='Script for video stabilization estimation'
+        description='Script for estimation of video stabilization.'
+        'Single avialable video extention is .mp4'
     )
-    parser.add_argument('path', metavar='videofile', type=str,
-                        help='path to videofile to estimate')
+    parser.add_argument(
+        '-p',
+        '--path',
+        metavar='videofile',
+        type=str,
+        help='.mp4 file path'
+    )
+    parser.add_argument(
+        '-s',
+        '--split_video',
+        action='store_true',
+        help='Divides each frame into four,'
+        ' getting four new videos.'
+        ' Evaluates each of them and averages the score.'
+        ' Improves accuracy, increases evaluation time.'
+    )
+    parser.add_argument(
+        '-e',
+        '--estimation_cnn',
+        metavar='seresnet18',
+        type=str,
+        default='seresnet18',
+        help='Pretrained cnn to estimate video stabilization.'
+        ' Default seresnet18.'
+        ' Avialable: seresnet18, seresnet50, resnet50'
+    )
 
     args = parser.parse_args()
-    print(args.path)
+    
